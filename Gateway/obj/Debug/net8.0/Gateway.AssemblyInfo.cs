0a487421db198b3e112230ec1b7029b33a1b2279
paoascanio <131598223+paoascanio@users.noreply.github.com>
2025-07-01 17:58:16 -0400

    feat nuevos puertos

--- Gateway/obj/Debug/net8.0/Gateway.AssemblyInfo.cs
+++ Gateway/obj/Debug/net8.0/Gateway.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Gateway")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6de3b281bca045a6def0102f4d6859fadae7a78")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc6e7c33dc1ace12ef65473b34d86e0c077824eb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Gateway")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Gateway")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
